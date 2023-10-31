f069c340c4961b392455f5e1f08a8a7b2cd8c8ba
Andrew Kane <andrew@ankane.org>
2023-10-31 00:36:42 -0700

    Improved code

--- example.nim
+++ example.nim
@@ -8,12 +8,12 @@ db.exec(sql"DROP TABLE IF EXISTS items")
 
 db.exec(sql"CREATE TABLE items (id bigserial PRIMARY KEY, embedding vector(3))")
 
-let embedding1 = @[1, 1, 1];
-let embedding2 = @[1, 1, 2];
-let embedding3 = @[2, 2, 2];
+let embedding1 = @[1, 1, 1]
+let embedding2 = @[1, 1, 2]
+let embedding3 = @[2, 2, 2]
 db.exec(sql"INSERT INTO items (embedding) VALUES (?), (?), (?)", %* embedding1, %* embedding2, %* embedding3)
 
-let embedding = @[1, 1, 1];
+let embedding = @[1, 1, 1]
 let rows = db.getAllRows(sql"SELECT * FROM items ORDER BY embedding <-> ? LIMIT 5", %* embedding)
 for row in rows:
   echo row[0], ": ", parseJson(row[1])
